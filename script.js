83feabf15b033e7f01231f137e5bba7c783be36d
ElmarCR7 <johncino082@gmail.com>
2024-04-11 18:27:04 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,227 @@
+const array = [
+    {
+        name:'Football',
+        FirstMatch: '1863 ',
+        FirstClub: 'Sheffield',
+        FirstMatch: 'Hallam VS Sheffield ',
+        FirstWC: '1930',
+        FirstWC: 'Uruguay VS Argentina',
+        FirstWinner:'Uruguay',
+        FirstChampLeague:'1955',
+        FirstChampLeague: 'Real Madrid VS Raims',
+        FirstChampLeagueWinner: 'Real Madrid',
+        GOAT: 'Crishtiano Ronaldo',
+        clubs:[
+    {
+    club: 'RealMadrid',
+    ChampLeague: '14',
+    Laliga: '35' ,
+    SuperCup: '5' , 
+    LeagueEurope: '2',
+    Stadium: 'Santiago Bernabéu ',
+    SpanishCup: '20',
+    Coach: 'Anciloti',
+    Capitain: 'Luca Modrich',
+    ClubWorldCup: '5',
+    Value:'1.04b',
+    Country: 'Spain',
+    City: 'Madrid',
+    Enemy: 'Atletico Madrid' ,
+    HowOld:'80',
+    Position: '1'
+},
+
+
+{
+    club: 'Manchester United',
+    ChampLeague: '3',
+    EPL: '20' ,
+    SuperCup: '1' , 
+    LeagueEurope: '1',
+    Stadium: 'Old Traford ',
+    FACup: '20',
+    Coach: 'Ten Hag',
+    Capitain: 'Maguer',
+    ClubWorldCup: '1',
+    Value:'734 m',
+    Country: 'England',
+    City: 'Manchester',
+    Enemy: 'Manchester City',
+    HowOld: '32 y.o',
+    Position: '6'
+
+},
+
+
+{
+
+    club: 'Juventus',
+    ChampLeague: '2',
+    SerieA: '36' ,
+    SuperCup: '2' , 
+    LeagueEurope: '3',
+    Stadium: 'Allianz ',
+    ItalianCup: '14',
+    Coach: 'Massimiliano Allegri',
+    Capitain: 'Vlahovich',
+    ClubWorldCup: '0',
+    Value:'490 m',
+    Country: 'Italy',
+    City: 'Torin',
+    Enemy: 'Torin',
+    HowOld: '49 y.o',
+    Position: '3'
+
+
+},
+
+
+{
+    Club: 'Bayern Munich',
+    ChampLeague: '6',
+    Bundesleague: '33' ,
+    SuperCup: '2' , 
+    LeagueEurope: '1',
+    Stadium: 'Allianz Arena ',
+    GERMANCup: '20',
+    Coach: 'Thomas Tuhel',
+    Capitain: 'Neuer',
+    ClubWorldCup: '2',
+    Value:'929 m',
+    Country: 'German',
+    City: 'Munich',
+    Enemy: 'Dortmunt',
+    HowOld: '59 y.o',
+    Position: '2'
+
+},
+
+
+{
+    club: 'Barcelona',
+    ChampLeague: '5',
+    Laliga: '27' ,
+    SuperCup: '5' , 
+    LeagueEurope: '0',
+    Stadium: 'Camp nou ',
+    SpanishCup: '31',
+    Coach: 'Xavi',
+    Capitain: 'Robert Arajo',
+    ClubWorldCup: '3',
+    Value:'840 b',
+    Country: 'Spain',
+    City: 'Catalonia',
+    Enemy: 'Real Madrid',
+    HowOld: '85 y.o',
+    Position: '2'
+},
+
+
+
+{
+    club: 'Liverpool',
+    ChampLeague: '6',
+    EPL: '19' ,
+    SuperCup: '4' , 
+    LeagueEurope: '3',
+    Stadium: 'Anfield ',
+    FACup: '8',
+    Coach: 'Klop',
+    Capitain: 'Salah',
+    ClubWorldCup: '1',
+    Value:'921 m',
+    Country: 'England',
+    City: 'Liverpool',
+    Enemy: 'Manchester City',
+    HowOld: '32 y.o',
+    Position: '2'
+},
+
+
+{
+    club: 'Milan',
+    ChampLeague: '2',
+    SerieA: '' ,
+    SuperCup: '2' , 
+    LeagueEurope: '3',
+    Stadium: 'Allianz ',
+    ItalianCup: '14',
+    Coach: 'Massimiliano Allegri',
+    Capitain: 'Theo Hernandez',
+    ClubWorldCup: '0',
+    Value:'490 m',
+    Country: 'Italy',
+    City: 'Milan',
+    Enemy: 'Inter Milan',
+    HowOld: '49 y.o',
+    Position: '2'
+},
+
+
+{
+    Club: 'Dortmunt',
+    ChampLeague: '2',
+    Bundesleague: '20' ,
+    SuperCup: '2' , 
+    LeagueEurope: '1',
+    Stadium: 'Arena ',
+    GERMANCup: '20',
+    Coach: 'Thomas Tuhel',
+    Capitain: 'Reus',
+    ClubWorldCup: '2',
+    Value:'929 m',
+    Country: 'German',
+    City: 'Dortmund',
+    Enemy: 'Bayern Munich',
+    HowOld: '55 y.o',
+    Position: '4'
+
+},
+
+
+{
+    club: 'Atletico Madrid',
+    ChampLeague: '0',
+    Laliga: '1' ,
+    SuperCup: '3' , 
+    LeagueEurope: '3',
+    Stadium: 'Civitas Metropolitano  ',
+    SpanishCup: '10',
+    Coach: 'Simeone',
+    Capitain: 'Koke',
+    ClubWorldCup: '0',
+    Value:'417 m',
+    Country: 'Spain',
+    City: 'Madrid',
+    Enemy: 'Real Madrid' ,
+    HowOld:'70',
+    Position: '4'
+
+},
+
+
+{
+    club: 'Chelsea',
+    ChampLeague: '2',
+    EPL: '6' ,
+    SuperCup: '2' , 
+    LeagueEurope: '2',
+    Stadium: ' Stamford Bridge ',
+    FACup: '8',
+    Coach: 'Tuhel',
+    Capitain: 'Sterling',
+    ClubWorldCup: '1',
+    Value:'928 m',
+    Country: 'England',
+    City: 'London',
+    Enemy: 'Arsenal',
+    HowOld: '32 y.o',
+    Position: '9'
+}
+
+]
+
+}
+
+
+]
